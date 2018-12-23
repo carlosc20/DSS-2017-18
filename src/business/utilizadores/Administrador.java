bc33ec97576aee451a5f81f91882e6d7862369d2
angelo <angeloandre1998@gmail.com>
2018-12-23 16:30:53 +0000

    Construtores Extends

--- src/business/utilizadores/Administrador.java
+++ src/business/utilizadores/Administrador.java
@@ -4,4 +4,12 @@ public class Administrador extends Utilizador {
     public Administrador(){
         super();
     }
+
+    public Administrador(String nome, String pass){
+        super(nome,pass);
+    }
+
+    public Administrador(Utilizador u){
+        super(u);
+    }
 }
