1d8e5ec1a0dcc052565ce3230783109aa46016ab
Daniel Vilar <daniel.costa1998@gmail.com>
2018-12-27 15:41:15 +0000

    Adiciona metodos list

--- src/data/PacoteDAO.java
+++ src/data/PacoteDAO.java
@@ -48,6 +48,25 @@ public class PacoteDAO extends DAO {
 		return list;
 	}
 
+	public List<Pacote> list(Componente componente) throws SQLException {
+		int idComponente = componente.getId();
+		Connection cn = Connect.connect();
+		List<Pacote> result = new ArrayList<>();
+		PreparedStatement st = cn.prepareStatement(
+				"SELECT id, designacao, desconto FROM Pacote" +
+						"INNER JOIN Pacote_Componente ON Pacote.id = Pacote_Componente.id_pacote" +
+						"WHERE Pacote_Componente.id_componente = ?");
+		st.setInt(1, idComponente);
+		ResultSet res = st.executeQuery();
+		while (res.next()){
+			int id = res.getInt("id");
+			String designacao = res.getString("designacao");
+			int desconto = res.getInt("desconto");
+			result.add(new Pacote(id, designacao, desconto, null));
+		}
+		return result;
+	}
+
 	public Pacote get(int id) throws SQLException {
 		Connection cn = Connect.connect();
 		PreparedStatement st = cn.prepareStatement("SELECT designacao, desconto FROM Pacote WHERE id = ? LIMIT 1");
@@ -70,46 +89,5 @@ public class PacoteDAO extends DAO {
 
 	public int size() throws SQLException {
 		return super.size("Pacote");
-	}
-
-	public Set<Pacote> getPacotesComComponente(int idComponente) throws SQLException {
-		Connection cn = Connect.connect();
-		Set<Pacote> result = new HashSet<>();
-		PreparedStatement st = cn.prepareStatement(
-				"SELECT id, designacao, desconto FROM Pacote" +
-				"INNER JOIN Pacote_Componente ON Pacote.id = Pacote_Componente.id_pacote"
-				+ "WHERE Pacote_Componente.id_componente = ?");
-		st.setInt(1, idComponente);
-		ResultSet res = st.executeQuery();
-		while (res.next()){
-			int id = res.getInt("id");
-			String designacao = res.getString("designacao");
-			int desconto = res.getInt("desconto");
-			result.add(new Pacote(id, designacao, desconto, null));
-		}
-		return result;
-	}
-
-	public Set<Componente> getComponentesPacote(int idPacote) throws SQLException {
-		Connection cn = Connect.connect();
-		Set<Componente> result = new HashSet<>();
-		PreparedStatement st = cn.prepareStatement("SELECT id_componente FROM Pacote_Componente WHERE id_pacote = ?");
-		st.setInt(1, idPacote);
-		ResultSet res = st.executeQuery();
-		ComponenteDAO componenteDAO =  new ComponenteDAO();
-		while (res.next()){
-			int id = res.getInt("id_componente");
-			result.add(componenteDAO.get(id));
-		}
-		return result;
-	}
-
-	public Set<Pacote> getPacotesCorrespondentes(Set<Componente> componentes) throws SQLException {
-		// Não sei o que é para fazer aqui!!!
-		Set<Pacote> result = new HashSet<>();
-		for (Componente componente:componentes){
-			result.addAll(getPacotesComComponente(componente.getId()));
-		}
-		return result;
 	}
 }
