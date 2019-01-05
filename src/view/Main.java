6ea230fef02ac3f322cf751a28e425617f77c7eb
Carlos Castro <cpac20.4@gmail.com>
2019-01-05 18:32:08 +0000

    asdsfaf

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,20 @@
 package view;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("GTK+".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }
+        catch(Exception e){
+            System.err.println("Error on changing the theme!");
+        }
         new Inicial();
     }
 }
